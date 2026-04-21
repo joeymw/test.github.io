3d0ffadbc15c78b947ea96f95b04c897eeee11ba
joeymw <wym011103@gmail.com>
2026-04-21 08:02:37 +0100

    Update and rename config.js to config.example.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  SUPABASE_URL: "https://onnovsqelgftttfuejoy.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ubm92c3FlbGdmdHR0ZnVlam95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMTc4MjYsImV4cCI6MjA4ODU5MzgyNn0.m-VR_3SDChpcRRk901bHJ891EEA8m1fd4H5c-CQdLDE"
-};
